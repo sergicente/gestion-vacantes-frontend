229772751316a58af9d81c12342c306699b8d645
Sergi Centellas <sergicente@icloud.com>
2025-04-25 19:59:38 +0200

    implementado aplicacion a vacante y listado de aplicaciones a vacantes

---
+++ src/app/services/solicitud.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, lastValueFrom, Observable, throwError } from 'rxjs';
+import { Ivacante } from '../interfaces/ivacante';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolicitudService {
+
+  http = inject(HttpClient);
+  private baseUrl: string = 'http://localhost:9001/api/solicitudes'
+  
+
+  constructor() { };
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.baseUrl);
+  }
+
+  getByEmail(email: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/usuario/${email}`);
+  }
+
+  cancelarSolicitud(idSolicitud: number): Observable<void> {
+    return this.http.put<void>(`${this.baseUrl}/${idSolicitud}/cancelar`, {});
+  }
+
+  getById(id: String): Observable<Ivacante> {
+    return this.http.get<any>(this.baseUrl + '/' + id);
+  }
+
+  modificar(id: string, item: any): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/modificar/${id}`, item).pipe(
+      catchError((error) => {
+        if (error.status === 400) {
+          alert('Error: El ID en la URL y en el body no coinciden.');
+        } else if (error.status === 404) {
+          alert('Error: No existe.');
+        } else {
+          alert('Error inesperado al modificar.');
+        }
+        return throwError(error);
+      })
+    );
+  }
+
+  borrar(id: String): Observable<any> {
+    console.log('Se esta borrando el item con id ' + id);
+    return this.http.delete<any>(this.baseUrl + '/borrar/' + id);
+  }
+
+  nuevo(datos: any) {
+    return this.http.post<any>(this.baseUrl + '/enviarSolicitud', datos).pipe(
+      catchError((error) => {
+        if (error.status === 409) {
+          alert('Ya existe con este nombre');
+        } else {
+          alert('Error al insertar.');
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
