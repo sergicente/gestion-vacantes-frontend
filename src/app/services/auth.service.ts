7e625858a3332f16ef0486330563f6819371f621
Sergi Centellas <sergicente@icloud.com>
2025-04-30 19:52:04 +0200

    fixed auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,30 @@
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-//   private user: { email: string } | null = null;
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private user: { email: string, rol?: string } | null = null;
 
-//   setUser(user: { email: string }) {
-//     this.user = user; // Guarda el usuario
-//   }
+  setUser(user: { email: string, rol?: string }) {
+    this.user = user; // Guarda el usuario
+  }
 
-//   getUser() {
-//     return this.user; // Retorna el usuario actual o null
-//   }
+  getUser() {
+    return this.user; // Retorna el usuario actual o null
+  }
 
-//   logout() {
-//     this.user = null; // Limpia el estado
-//   }
-// }
+  getRol(): string {
+    const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
+    return usuario.rol || '';
+  }
+
+  logout() {
+    this.user = null; // Limpia el estado
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
+}
+
