e6d0ba2657920ccbce66195255196967da9e4cf7
Sergi Centellas <sergicente@icloud.com>
2025-04-30 19:44:14 +0200

    Arreglado menu dashboard

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
-  private user: { email: string } | null = null;
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+//   private user: { email: string } | null = null;
 
-  setUser(user: { email: string }) {
-    this.user = user; // Guarda el usuario
-  }
+//   setUser(user: { email: string }) {
+//     this.user = user; // Guarda el usuario
+//   }
 
-  getUser() {
-    return this.user; // Retorna el usuario actual o null
-  }
+//   getUser() {
+//     return this.user; // Retorna el usuario actual o null
+//   }
 
-  logout() {
-    this.user = null; // Limpia el estado
-  }
-}
+//   logout() {
+//     this.user = null; // Limpia el estado
+//   }
+// }
 
